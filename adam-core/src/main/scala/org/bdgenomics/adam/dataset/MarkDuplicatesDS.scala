95b58809b3bd571145f89b904ab35cf6a618ce95
jpdna <justinpaschalldna@gmail.com>
2016-04-23 22:18:02 -0400

    testmod12 is working

---
+++ adam-core/src/main/scala/org/bdgenomics/adam/dataset/MarkDuplicatesDS.scala
@@ -0,0 +1,8 @@
+package org.bdgenomics.adam.dataset
+
+/**
+  * Created by jp on 4/14/16.
+  */
+class MarkDuplicatesDS {
+
+}
---
+++ adam-core/src/main/scala/org/bdgenomics/adam/dataset/SingleReadBucketKeyedByRefPosPairDS.scala
@@ -0,0 +1,33 @@
+package org.bdgenomics.adam.dataset
+
+import org.apache.spark.sql.{SQLContext, DataFrame}
+
+object SingleReadBucketKeyedByRefPosPairDS {
+
+  def apply(df: DataFrame, sqlContext: SQLContext) = {
+
+    import sqlContext.implicits._
+
+    val ds = df.as[AlignmentRecordLimitProjDS]
+
+    ds.groupBy(p => (p.recordGroupName, p.readName))
+      .mapGroups {
+        case ((recordGroupName, readName), reads: Iterator[AlignmentRecordLimitProjDS]) => {
+          val (mapped, unmapped) = reads.partition(_.readMapped)
+          val (primaryMapped, secondaryMapped) = mapped.partition(_.primaryAlignment)
+          new SingleReadBucketDS(primaryMapped.toSeq, secondaryMapped.toSeq, unmapped.toSeq)
+        }
+      }
+
+  }
+
+
+
+
+}
+
+
+case class SingleReadBucketKeyedByRefPosPairDS(
+                               referencePositionPair: ReferencePositionPairDS,
+                               singleReadBucket: SingleReadBucketDS,
+                               )
