fbc2de1bd80c57d2e3d4aade7d7f6bb3b8df6028
jpdna <justinpaschalldna@gmail.com>
2017-02-23 14:45:00 -0500

    Fixed parent pom version in adam-hbase

--- adam-hbase/src/main/scala/org/bdgenomics/adam/cli/Vcf2HBase.scala
+++ adam-hbase/src/main/scala/org/bdgenomics/adam/cli/Vcf2HBase.scala
@@ -24,7 +24,7 @@ import org.bdgenomics.adam.models.{ RecordGroupDictionary, SequenceDictionary, S
 import org.bdgenomics.adam.projections.{ AlignmentRecordField, Filter }
 import org.bdgenomics.adam.rdd.ADAMContext._
 import org.bdgenomics.adam.rdd.ADAMSaveAnyArgs
-import org.bdgenomics.adam.rdd.read.{ AlignedReadRDD, AlignmentRecordRDD, MDTagging }
+//import org.bdgenomics.adam.rdd.read.{ AlignedReadRDD, AlignmentRecordRDD, MDTagging }
 import org.bdgenomics.adam.rich.RichVariant
 import org.bdgenomics.formats.avro.AlignmentRecord
 import org.bdgenomics.utils.cli._
